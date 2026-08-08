5bae8f898162cd4c1a0a0692fec9ca80f987e8a0
samyakchadaga <samyakchada@gmail.com>
2026-08-08 11:33:01 +0530

    Refactor warp_state_table with enhanced comments
    Updated module comments and added last_mem logic for state management.

--- RTL/warp_state_table.sv
+++ RTL/warp_state_table.sv
@@ -1,22 +1,10 @@
 import state_pkg::*;
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Design Name: 
 // Module Name: Warp State Table
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Project Name: Warp-Scheduler
+// Target Devices: GPU
+// Tool Versions: Vivado 2024.1
 //////////////////////////////////////////////////////////////////////////////////
 
 module st_tab#(
@@ -29,28 +17,55 @@ module st_tab#(
  input scheduler_valid,
  input [NUM_WARPS-1:0]mem_return,
  input is_load, 
+ input last_inst,
  
  output logic [NUM_WARPS-1:0] warp_rd
     );
     
     
    warp_state state [0:NUM_WARPS-1];
+   logic [NUM_WARPS-1:0] last_mem;
     integer i ;
     
     always_ff @(posedge clk) begin 
     if(!rst) begin
-     for (int i = 0; i < NUM_WARPS; i++)
+     for (int i = 0; i < NUM_WARPS; i++)begin
         state[i] <= READY;
+        last_mem[i] <= 1'b0;
+        end
     end 
     
     else begin
      for (int i=0; i<NUM_WARPS; i++) begin
-            if (mem_return[i])
-           state[i] <= READY;
+              if (mem_return[i]) begin
+
+                if (last_mem[i]) begin
+                    state[i] <= DONE;
+                    last_mem[i] <= 1'b0;
+                end
+                else begin
+                    state[i] <= READY;
+                end
+
+            end
         end
 
-        if (scheduler_valid && is_load)
+
+
+        if (scheduler_valid)begin
+        
+        if(is_load)begin
         state[issued_warp] <= MEM_WAIT;
+        
+        if (last_inst)
+                    last_mem[issued_warp] <= 1'b1;
+          end 
+          else if (last_inst) begin
+
+                state[issued_warp] <= DONE;
+
+            end
+            end
   end  
 end   
 
