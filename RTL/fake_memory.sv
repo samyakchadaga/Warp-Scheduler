b7972d7b637288555f22913a7dbf32297085aa70
samyakchadaga <samyakchada@gmail.com>
2026-08-08 11:36:57 +0530

    Revise header comments in fake_memory.sv
    Updated module and project metadata in fake_memory.sv

--- RTL/fake_memory.sv
+++ RTL/fake_memory.sv
@@ -1,22 +1,9 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/17/2026 09:18:10 PM
-// Design Name: 
-// Module Name: hhhh
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Module Name: Fake Memory
+// Project Name: Warp Scheduler
+// Target Devices: GPU
+// Tool Versions: Vivado 2024.1
 //////////////////////////////////////////////////////////////////////////////////
 
 module fk_mem#(
